f093e2d9bfae7d4ecbf9ed3446b4e2f79d8c0826
TajnimaBintaNaz <tajnimavu10@gmail.com>
2022-02-01 14:50:12 +0600

    all file added

---
+++ variable-operation.js
@@ -0,0 +1,7 @@
+var eggprice = 20;
+var onionprice = 10;
+
+var totalprice = eggprice + onionprice;
+var pricediffrence= eggprice - onionprice;
+
+console.log(totalprice) 
---
+++ variable.js
@@ -0,0 +1,9 @@
+ var price = 21;
+ var age = 22;
+ var temperature =37;
+
+ var name = 'abony';
+ var address = 'vooter goli kochu khet';
+
+var pass = true;
+var subsribed = false; 
